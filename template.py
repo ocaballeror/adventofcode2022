887d6f4809ed979c4d7614471ca2894dd2112b2b
Oscar Caballero <oscar.caballero@bayes.gg>
2024-12-31 17:31:15 +0100

    Add template

---
+++ template.py
@@ -0,0 +1,18 @@
+# type: ignore
+
+def read_input():
+    with open("input") as f:
+        pass
+
+
+def part1():
+    data = read_input()
+
+
+def part2():
+    data = read_input()
+
+
+if __name__ == "__main__":
+    print("Part 1:", part1())
+    print("Part 2:", part2())
